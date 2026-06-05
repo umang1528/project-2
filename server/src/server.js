5ef30a11b2b0280077415ed9503cb82db943f212
UMANG <hellohaji665@gmail.com>
2026-06-05 17:28:25 +0530

    corrected env

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ import app from './app.js';
 import { connectDB } from './config/db.js';
 import { env } from './config/env.js';
 
-const PORT = env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 async function startServer() {
   await connectDB();
